48162e5ce156dc15406496678fafd562ba8c77b8
AlCapone1234 <xxglobalelitexx123@gmail.com>
2026-04-08 20:38:18 +0200

    Added: Initial project for our magnetic sensors, and some initial code aswell.

---
+++ magnetic-sensor/main/magnetic-sensor.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_chip_info.h"
+#include "esp_heap_caps.h"
+#include "driver/gpio.h"
+
+#define SENSOR_GPIO_PORT GPIO_NUM_2
+#define BYTES_TO_WORD(x) (x/4)
+
+static const char* TAG = "Door/Window Sensor";
+
+void sensor_read_task(void* params)
+{
+	while (true)
+	{
+		int state = gpio_get_level(SENSOR_GPIO_PORT);
+
+		if (state == 0)
+			ESP_LOGE(TAG, "CLOSED!");
+		else
+			ESP_LOGE(TAG, "OPEN!");
+		
+		vTaskDelay(100);
+	}
+}
+
+void app_main(void)
+{
+	gpio_config_t config = {
+		.pin_bit_mask = (1ULL << SENSOR_GPIO_PORT),
+		.mode = GPIO_MODE_INPUT,
+		.pull_up_en = GPIO_PULLUP_ENABLE,
+		.pull_down_en = GPIO_PULLDOWN_DISABLE,
+		.intr_type = GPIO_INTR_DISABLE
+	};
+	gpio_config(&config);
+
+	BaseType_t read_task_result = xTaskCreate(sensor_read_task, "ReadTask", BYTES_TO_WORD(4096), NULL, 10, NULL);
+	if (read_task_result != pdPASS)
+	{
+		// Task failed to be created
+		ESP_LOGE(TAG, "ReadTask failed to be created!");
+	}
+}
