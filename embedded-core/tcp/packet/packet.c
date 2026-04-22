d82a843ef59d710deb60cc9d5c7d55dd3d10d01d
AlCapone1234 <xxglobalelitexx123@gmail.com>
2026-04-22 12:38:38 +0200

    Added: Packet job conversion functions

--- embedded-core/tcp/packet/packet.c
+++ embedded-core/tcp/packet/packet.c
@@ -1,6 +1,49 @@
 #include "packet.h"
 
+#include <string.h>
+
 #include "../../json/cJSON.h"
+
+#define Packet_Job_Find_Job(str_value1, str_value2, return_value) \
+    do { \
+        if (strcmp((str_value1), (str_value2)) == 0) { \
+            return (return_value); \
+        } \
+    } while(0) \
+
+
+char* Packet_Job_To_String(Packet_Job job)
+{
+    switch (job)
+    {
+        case Packet_Job_Initialize:
+        {
+            return "Initialize"; 
+        }
+        case Packet_Job_Data:
+        {
+            return "Data"; 
+        }
+        case Packet_Job_Heartbeat:
+        {
+            return "Heartbeat"; 
+        }
+        default:
+        {
+            return NULL;
+        }
+    }
+}
+
+Packet_Job Packet_Job_From_String(const char* string)
+{
+    // Returns if job is found
+    Packet_Job_Find_Job(string, "Initialize", Packet_Job_Initialize);
+    Packet_Job_Find_Job(string, "Data", Packet_Job_Data);
+    Packet_Job_Find_Job(string, "Heartbeat", Packet_Job_Heartbeat);
+
+    return Packet_Job_Unknown;
+}
 
 char* Packet_Build(Packet_Job job, const char* message)
 {
--- embedded-core/tcp/packet/packet.h
+++ embedded-core/tcp/packet/packet.h
@@ -5,9 +5,14 @@ typedef enum
 {
     Packet_Job_Initialize,
     Packet_Job_Heartbeat,
-    Packet_Job_Data
+    Packet_Job_Data,
+    Packet_Job_Unknown
 } Packet_Job;
 
 char* Packet_Build(Packet_Job job, const char* message);
+
+char* Packet_Job_To_String(Packet_Job job);
+
+Packet_Job Packet_Job_From_String(const char* string);
 
 #endif
