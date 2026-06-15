7c16d1caee00a370fa347d49e92abe35b7a9a234
AlCapone1234 <xxglobalelitexx123@gmail.com>
2026-06-15 15:12:24 +0200

    Commented: Code

--- home-hub/main/commands/commands.h
+++ home-hub/main/commands/commands.h
@@ -1,9 +1,24 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+/**
+ * @brief Checks the fragmentation of the program
+ */
 int Command_Handle_Fragment(int argc, char **argv);
+
+/**
+ * @brief Gives information about all created tasks
+ */
 int Command_Handle_Tasks(int argc, char **argv);
+
+/**
+ * @brief Prints out how long ESP32 has been running
+ */
 int Command_Handle_Uptime(int argc, char **argv);
+
+/**
+ * @brief Restarts the ESP32
+ */
 int Command_Handle_Restart(int argc, char **argv);
 
 #endif
--- home-hub/main/sensor/sensor.h
+++ home-hub/main/sensor/sensor.h
@@ -30,10 +30,48 @@ typedef struct
     SensorPlacement placement;
 } Sensor;
 
+/**
+ * @brief Initializes the Sensor array
+ */
 void Sensor_Initialize_All();
+
+/**
+ * @brief Adds a Sensor to the array.
+ *
+ * Adds a Sensor to the array, Array size should be less than MAX_SENSORS otherwise it will fail to add.
+ *
+ * @param type The type of the Sensor.
+ * @param guid The UUID of the Sensor to add.
+
+ * @note guid means exact same as UUID in this case
+ *
+ */
 void Sensor_Add(Sensor_Type type, char* guid);
+
+/**
+ * @brief Prints info about ALL the sensors
+ */
 void Sensor_Print_All();
+
+/**
+ * @brief Get a Sensor by UUID
+ *
+ * Loops through the Sensor array and checks if UUID is equal and then returns 
+ * the pointer to it.
+ * 
+ * @param uuid The UUID that needs to match the sensor that is looked for.
+ * @return Pointer to the sensor in the array.
+ */
 Sensor* Sensor_Get_By_UUID(const char* uuid);
+
+/**
+ * @brief Get the Sensor Array
+ *
+ * @return Pointer to the sensor array.
+ *
+ * @note This function returns a pointer of a singleton. 
+ *
+ */
 Sensor* Sensor_Get_All();
 
 
